799d487a725e6aa2195d0f9e9016a7ac35396034
Jayy von Kush <jtman112@gmail.com>
2024-04-11 13:44:07 +1200

    Splash Update;
    Added the update from the main file.

---
+++ src/splash/renderer.js
@@ -0,0 +1,31 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+const remote = require('electron').remote;
+const { ipcRenderer } = require('electron');
+
+const win = remote.getCurrentWindow(); /* Note this is different to the
+html global `window` variable */
+
+// When document has loaded, initialise
+document.onreadystatechange = (event) => {
+    if (document.readyState == "complete") {
+        handleWindowControls();
+
+        document.getElementById('electron-ver').innerHTML = `${process.versions.electron}`
+    }
+};
+
+window.onbeforeunload = (event) => {
+    /* If window is reloaded, remove win event listeners
+    (DOM element listeners get auto garbage collected but not
+    Electron win listeners as the win is not dereferenced unless closed) */
+    win.removeAllListeners();
+}
+
+function handleWindowControls() {
+    document.getElementById('close-button').addEventListener("click", event => {
+        win.close();
+        ipcRenderer.send('splash-closed');
+    });
+}
